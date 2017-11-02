cbf89ebe11b6bba942f0e02ccf72b003f17c65cf
hyper <hyperionos88@gmail.com>
2017-11-02 16:54:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
   } else if (string === string.toLowerCase()) {
     return"I can\'t hear you!";
-  } else if (string === "I love you, Grandma") {
+  } else if ("I love you, Grandma") {
     return "I love you, too";
   }
 }
